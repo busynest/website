1e6f74b5350ac2c7c76e2d1ec4f4bf5687fe4de2
Jack Markiewicz <jack.solutionbox@gmail.com>
2021-09-27 22:27:55 -0700

    '0.0.9'

--- webpack.config.js
+++ webpack.config.js
@@ -1,108 +1,107 @@
-'use strict';
-
-const webpack                 = require('webpack');
-const path                    = require('path');
-const BundleAnalyzerPlugin    = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-const HtmlWebpackPlugin       = require('html-webpack-plugin');
-const CleanWebpackPlugin      = require('clean-webpack-plugin');
-const BabelLoader             = require('babel-loader');
-
-
-//const UglifyJsPlugin          = require('uglifyjs-webpack-plugin');
-//const config                  = require('../app.config')(true);
-
-// the path(s) that should be cleaned
-let pathsToClean = [
-  'dist'
-]
-
-// the clean options to use
-let cleanOptions = {
-  root:     '/full/webpack/root/path',
-  exclude:  ['shared.js'],
-  verbose:  true,
-  dry:      false,
-  beforeEmit: true
-}
-
-module.exports =  {
-
-    mode: 'production',
-  
-    entry: {
-      "application-shell":  './src/application-shell.js',
-      "one-two":            './src/one-two.js',
-      "wrong-page":         './src/wrong-page.js',
-      "send-feedback":      './src/send-feedback.js'
-  },
-  
-    resolve: {
-      modules:    [ "node_modules", path.resolve(__dirname, 'node_modules') ],
-      extensions: [".js", ".json"]
-    },
-  
-    output: {
-      path: path.resolve(__dirname, 'dist'), filename: '[name].js',
-    },
-  
-    module: {
-      rules: [
-        { test: /\.html$/,  exclude: /(node_modules|bower_components)/, use: 'html-loader' },
-        { test: /\.js$/, use: 'babel-loader'},
-        { test: /\.json$/,  exclude: /(node_modules|bower_components)/, use: 'json-loader' }
-      ]
-    },
-  
-    plugins: [
-      
-      new BundleAnalyzerPlugin(),
-
-      // new CleanWebpackPlugin( [ config.outputDir ], { root: resolve(__dirname, '/src/') } ) ,
-      new CleanWebpackPlugin(pathsToClean, cleanOptions),
-
-      new HtmlWebpackPlugin({template: './index.html'}),
-/**
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          compress: true,
-          ecma: 6,
-          mangle: true
-        }
-      })
-
-      // new ScriptExtHtmlWebpackPlugin( { defer: [ 'webcomponents-loader.js' ], } ),
- 
-      // copy custom static assets
-      new CopyWebpackPlugin([
-        {
-          from: resolve(__dirname, './node_modules/@webcomponents/webcomponentsjs/*.js'),
-          to: './scripts/wc',
-          ignore: ['webcomponents-loader.js'],
-          flatten: true,
-        },
-      ]),
-
-      new HtmlWebpackPlugin( {
-        config,
-        minify: {
-          collapseWhitespace: true,
-          removeComments: true,
-          removeRedundantAttributes: true,
-          removeScriptTypeAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          sortAttributes: true,
-          sortClassName: true,
-          useShortDoctype: true,
-          minifyCSS: true,
-          minifyJS: true,
-        },
-        hash: true,
-        inject: true,
-      }),
+/*
+import CopyWebpackPlugin  from 'copy-webpack-plugin';
+import HtmlWebpackPlugin  from 'html-webpack-plugin';
+import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
 */
+const CopyWebpackPlugin     = require('copy-webpack-plugin');
+const HtmlWebpackPlugin     = require('html-webpack-plugin');
+const WorkboxWebpackPlugin  = require('workbox-webpack-plugin');
+
+module.exports = {
+
+  // DEVELOPMENT
+  devServer: {
+    historyApiFallback: true
+  },
+
+  // INPUT
+  entry: path.resolve(__dirname, 'deploy/application-shell.js'),
+
+  // OUTPUT
+  output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'build'),
+  },
+
+  // RESOLVE
+  resolve: {
+    extensions: ['.js', '.ts'],
+    modules: [
+      path.resolve(__dirname, 'node_modules')
     ],
-  
-  };
+  },
+
+  // BUILD
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        // EXCLUDE
+        exclude: {
+          exclude: [
+   
+          ]
+        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {targets: {ie: '11'}}]
+            ],
+            plugins: ['@babel/plugin-syntax-dynamic-import']
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+
+  // PLUGINS
+  plugins: [
+    
+    new CopyWebpackPlugin([
+      'images/**',
+      'node_modules/@webcomponents/webcomponentsjs/**',
+      'manifest.json',
+      'polymer.json',
+      'sitemap.xml',
+      'robots.txt',
+      'ads.txt',
+      'firebase-messaging-sw.js',
+      'TERMS.md'
+    ]),
+
+    new HtmlWebpackPlugin({
+      chunksSortMode: 'none',
+      template: 'index.html'
+    }),
+/*
+    new InjectManifest({
+      swSrc: './src/sw.js',
+    }),
+*/
+    new WorkboxWebpackPlugin.GenerateSW({
+      include: ['index.html', 'manifest.json', /\.js$/],
+      exclude: [/\/@webcomponents\/webcomponentsjs\//],
+      navigateFallback: 'index.html',
+      swDest: 'sw.js',
+      clientsClaim: true,
+      skipWaiting: true,
+      runtimeCaching: [
+        {
+          urlPattern: /\/@webcomponents\/webcomponentsjs\//,
+          handler: 'staleWhileRevalidate'
+        },
+        {
+          urlPattern: /^https:\/\/fonts.gstatic.com\//,
+          handler: 'staleWhileRevalidate'
+        }
+      ]
+    })
+
+  ]
+};
